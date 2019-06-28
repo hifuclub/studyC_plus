b6fbc32c9abe94e6e6dd5dd826b8902d849f656a
sym <3359937762@qq.com>
2019-06-28 22:14:59 +0800

    inte

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,78 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+//class BankCustomer {
+//	BankCustomer(const BankCustomer& A){};
+//	BankCustomer operator= (BankCustomer A) {};
+//	bool operator== (BankCustomer A) {};
+//	BankCustomer operator+ (BankCustomer A) {};
+//
+//};
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+class IObject {
+public:
+	IObject(const IObject& o) {
+
+	}
+	IObject() {
+
+	}
+	virtual ~IObject() {
+
+	}
+};
+class Vehicle : IObject {
+
+	int mMaxPassengers;
+	float mMaxSpeed;
+	float mMaxWeight; 
+	bool LandVehicle;
+public:
+	int get_mMaxPassengers() { return mMaxPassengers; }
+	float get_mMaxSpeed() { return mMaxSpeed; }
+	float get_mMaxWeight() { return mMaxWeight; }
+	bool isLandVehicle() { return mMaxWeight; }
+	Vehicle(const Vehicle& o) {
+
+	}
+	Vehicle(int mMaxPassengers, float mMaxSpeed, float mMaxWeight) {
+		this->mMaxPassengers = mMaxPassengers;
+		this->mMaxSpeed = mMaxSpeed;
+		this->mMaxWeight = mMaxWeight;
+	}
+	virtual ~Vehicle() {
+
+	}
+};
+
+class  Car : public Vehicle {
+
+public:
+	Car(const Car& o) {
+
+	}
+	Car() :Vehicle(int mMaxPassengers, float mMaxSpeed, float mMaxWeight) {
+		Vehicle::isLandVehicle = true;
+	}
+	virtual ~Car() {
+
+	}
+};
+class Plane : public Vehicle {
+public:
+	Plane(const Plane& o) {
+
+	}
+	Plane() :Vehicle(int mMaxPassengers, float mMaxSpeed, float mMaxWeight) {
+		Vehicle::isLandVehicle = false;
+	}
+	virtual ~Plane() {
+
+	}
+};
---
+++ My_share_ptr/My_share_ptr.cpp
@@ -0,0 +1,52 @@
+﻿
+// My_share_ptr.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <memory.h>
+
+class MyObject {
+public:
+	MyObject(int id) :m_id(id) 
+	{
+		std::cout << "init obj" << m_id << std::endl;
+	}
+	~MyObject()
+	{
+		std::cout << "byebye" << m_id << std::endl;
+	}
+	int id() { return m_id; }
+private:
+	int m_id;
+};
+
+static void interfaceOfSharedPtr();
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	interfaceOfSharedPtr();
+
+
+}
+
+void interfaceOfSharedPtr() {
+	std::shared_ptr<MyObject> null;
+	std::shared_ptr<MyObject> obj1(new MyObject(1));
+	std::shared_ptr<MyObject> obj2(obj1);
+	std::cout << obj1.use_count() << std::endl;
+	std::cout << obj2.use_count() << std::endl;
+	std::cout << "obj1==null" << std::endl;
+	obj1 = null;
+	std::cout << obj1.use_count() << std::endl;
+	std::cout << obj2.use_count() << std::endl;
+	std::cout << "obj2==null" << std::endl;
+	obj2 = null;
+	std::cout << obj1.use_count() << std::endl;
+	std::cout << obj2.use_count() << std::endl;
+}
+
+
+
+
